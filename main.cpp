4db0d490cab30b6795a89fa22a34833bcf51d864
klyasota <konstantin.lyasota@mundfish.com>
2023-09-22 01:19:29 +0400

    Add example of joining iterators of the same type, make output values easier to distinct between different underlying iterators

--- main.cpp
+++ main.cpp
@@ -16,14 +16,18 @@ namespace detail{
 int main() {
     std::vector<int> i;
     i.emplace_back(1);
-    i.emplace_back(12);
-    i.emplace_back(13);
-    i.emplace_back(14);
+    i.emplace_back(2);
+    i.emplace_back(3);
+    i.emplace_back(4);
     std::map<int, float> m;
     m.emplace(15, 34);
-    m.emplace(230, 34);
-    m.emplace(61, 34);
-    join_container_t join_c(type_holder_t<int>{}, i, m);
+    m.emplace(16, 34);
+    m.emplace(17, 34);
+    std::vector<int> i1;
+    i1.emplace_back(101);
+    i1.emplace_back(102);
+    join_container_t join_c(type_holder_t<int>{}, i, m, i1);
+    join_container_t join_c1 = join_c;
     for (auto&& v : join_c) {
         std::cout << v << std::endl;
     }
